4509567070240ac64bb9b6dbf1e21867eb9e7cf8
Kadu <kadu.silva2014@gmail.com>
2024-07-24 11:41:12 -0300

    ref: replacing layout with div with table

---
+++ src/components/EmployeeRow.ts
@@ -0,0 +1,29 @@
+import { formatDateToDDMMYYYY, formatPhoneNumber } from "../helpers/formatters";
+import { Employee } from "../types/responses";
+import './employeeRow.css';
+
+export function createEmployeeRow(employee: Employee): HTMLDivElement {
+  const { id, image, admission_date, job, name, phone } = employee;
+
+  const divRow = document.createElement('tr');
+  divRow.classList.add('table-item-row', 'row-grid');
+  divRow.id = `${id}`;
+
+
+  divRow.innerHTML = `
+    <td class="photo-grid table-row-photo"><img src="${image}"
+        alt="Foto de Giovana L. Arruda"></td>
+    <td class="name-grid">${name}</td>
+    <td data-head="Cargo" class="job-grid details-content details-${id}">${job}</td>
+    <td data-head="Data de Admissão" class="date-grid details-content details-${id}">${formatDateToDDMMYYYY(admission_date)}</td>
+    <td data-head="Telefone" class="phone-grid details-content details-${id}">${formatPhoneNumber(phone)}</td>
+    <td class="toggle-icon-grid details-content-icon">
+      <button data-details="${id}" class="toggle-details">
+        <i data-lucide="chevron-down"></i>
+      </button>
+    </td>
+  `
+
+
+  return divRow
+}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,31 @@
 import './style.css';
 import { startIcons } from './lucideIcons';
+import { createEmployeeRow } from './components/EmployeeRow';
 
 startIcons();
+
+const tableBody = document.querySelector('tbody');
+
+tableBody?.appendChild(createEmployeeRow({
+  admission_date: '2021-01-01',
+  id: 1,
+  image: 'https://picsum.photos/500/300?random=1',
+  job: 'Desenvolvedor',
+  name: 'João',
+  phone: '5587996242198'
+}))
+
+const buttonsActions = document.querySelectorAll<HTMLButtonElement>('.toggle-details');
+
+buttonsActions.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    const rowId = button.getAttribute('data-details');
+    const details = document.querySelectorAll<HTMLTableCellElement>(`.details-${rowId}`);
+
+    details.forEach((detail) => {
+      detail.classList.toggle('details-active');
+    })
+  })
+})
+
+startIcons()
