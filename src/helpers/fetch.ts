bc0003534ed422bf1d9ad833d49804a9b847f739
Kadu <kadu.silva2014@gmail.com>
2024-07-24 16:48:35 -0300

    Chore: get the url for request in the environment

--- src/helpers/fetch.ts
+++ src/helpers/fetch.ts
@@ -1,7 +1,8 @@
+import { ENV } from '../env'
 import { Employee } from '../types/responses'
 
 export async function getEmployees(): Promise<Employee[]> {
-  const response = await fetch('http://localhost:3000/employees')
+  const response = await fetch(`${ENV.VITE_APP_API_URL}/employees`)
   const employees = await response.json()
   return employees as Employee[]
 }
