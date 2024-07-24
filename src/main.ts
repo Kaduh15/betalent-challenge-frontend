f57166606c512f561f9207619c57054fa0d88ae2
Kadu <kadu.silva2014@gmail.com>
2024-07-24 18:44:18 -0300

    Feat: implements filter logic by input value

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,34 @@ import './style.css'
 import { startIcons } from './libs/lucideIcons'
 import { createEmployeeRow } from './components/EmployeeRow'
 import { getEmployees } from './helpers/fetch'
+import { Employee } from './types/responses'
 
 startIcons()
 
 const tableBody = document.querySelector('.table-body')
+const inputSearch = document.querySelector('#search-input')
 
-getEmployees().then((employees) => {
+let employees: Employee[] = []
+
+getEmployees().then((employeesResponse) => {
+  employees = employeesResponse
   employees.forEach((employee) => {
     const employeeRow = createEmployeeRow(employee)
     tableBody?.appendChild(employeeRow)
   })
+  startIcons()
+})
+
+inputSearch?.addEventListener('input', (e) => {
+  const search = (e.target as HTMLInputElement).value.toLowerCase()
+  if (tableBody) {
+    tableBody.innerHTML = ''
+    employees.forEach((employee) => {
+      if (employee.name.toLowerCase().includes(search)) {
+        tableBody.appendChild(createEmployeeRow(employee))
+      }
+    })
+  }
+
   startIcons()
 })
